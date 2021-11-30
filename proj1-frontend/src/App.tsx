3b6610864a097826214d75a15b38ed4f864fb2ea
Albert Tian <atian@berkeley.edu>
2021-11-30 12:34:30 -0500

    content: connect wallet button

--- proj1-frontend/src/App.tsx
+++ proj1-frontend/src/App.tsx
@@ -1,36 +1,39 @@
 import * as React from "react";
 import {
-  ChakraProvider,
   Box,
   Text,
   Link,
   VStack,
   Code,
   Grid,
-  theme,
+  Button,
+  useToast,
 } from "@chakra-ui/react";
 import { ColorModeSwitcher } from "./ColorModeSwitcher";
 import { Logo } from "./Logo";
 import { useEffect, useState } from "react";
+// import detectEthereumProvider from "@metamask/detect-provider";
 
 export const App = () => {
   // State to store currentAccount
   const [currentAccount, setCurrentAccount] = useState("");
+  const toast = useToast();
 
   // To check if the wallet is connected (if etherium exists on window)
   const checkWalletConnection = async () => {
     try {
-      // To check if we actually have metamast
-      const { ethereum } = window;
-      if (!ethereum) {
+      // To check if we actually have metamask
+      // const provider = (await detectEthereumProvider()) as any;
+      const provider = window.ethereum;
+      if (!provider) {
         console.log("Make sure you have metamask!");
         return;
       } else {
-        console.log("We have the ethereum object", ethereum);
+        console.log("We have the provider object", provider);
       }
 
       // Check if we have authorization
-      const accounts = await ethereum.request({ method: "eth_accounts" });
+      const accounts = await provider.request({ method: "eth_accounts" });
 
       // Get first account (?)
       if (accounts.length !== 0) {
@@ -46,6 +49,35 @@ export const App = () => {
     }
   };
 
+  const connectWallet = async () => {
+    try {
+      // const provider = (await detectEthereumProvider()) as any;
+      const provider = window.ethereum;
+      if (!provider) {
+        // alert("Make sure you have metamask!");
+        toast({
+          title: "Make sure you have metamask!",
+          description: "Get the chrome extension to connect your wallet",
+          status: "error",
+          duration: 9000,
+          isClosable: true,
+        });
+        return;
+      } else {
+        console.log("We have the provider object", provider);
+      }
+
+      const accounts = await provider.request({
+        method: "eth_requestAccounts",
+      });
+
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   // Run on load
   useEffect(() => {
     checkWalletConnection();
@@ -54,27 +86,28 @@ export const App = () => {
   }, []);
 
   return (
-    <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
-    </ChakraProvider>
+    <Box textAlign="center" fontSize="xl">
+      <Grid minH="100vh" p={3}>
+        <ColorModeSwitcher justifySelf="flex-end" />
+        <VStack spacing={8}>
+          <Logo h="40vmin" pointerEvents="none" />
+          <Text>
+            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
+          </Text>
+          <Link
+            color="teal.500"
+            href="https://chakra-ui.com"
+            fontSize="2xl"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn Chakra
+          </Link>
+          {!currentAccount && (
+            <Button onClick={connectWallet}>Connect Metamask</Button>
+          )}
+        </VStack>
+      </Grid>
+    </Box>
   );
 };
