3904427301f6fb5bc1bc750f352cfc5362b52b61
Albert Tian <atian@berkeley.edu>
2021-12-08 19:12:09 -0800

    misc: better all meta

--- proj1/frontend/src/pages/_document.tsx
+++ proj1/frontend/src/pages/_document.tsx
@@ -9,15 +9,41 @@ export default class Document extends NextDocument {
           <meta charSet="utf-8" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
           <meta name="author" content="Albert Tian" />
+          <title>web3 playground</title>
           <meta
             name="description"
             content="a web3 playground for my personal projects from buildspace."
           />
-          <meta name="keywords" content="web3, ethereum, buildspace" />
-          <link rel="shortcut icon" href="/static/meta/favicon.png" />
-          <meta property="og:image" content="/static/meta/ogImage.png" />
-          <meta property="og:title" content="web3 playground" />
+
+          {/* <!-- Facebook Meta Tags --/> */}
+          <meta property="og:url" content="https://web3-albtian.vercel.app/" />
           <meta property="og:type" content="website" />
+          <meta property="og:title" content="web3 playground" />
+          <meta
+            property="og:description"
+            content="a web3 playground for my personal projects from buildspace."
+          />
+          <meta
+            property="og:image"
+            content="https://web3-albtian.vercel.app/static/meta/ogImage.png"
+          />
+
+          {/* <!-- Twitter Meta Tags --/> */}
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta property="twitter:domain" content="web3-albtian.vercel.app" />
+          <meta
+            property="twitter:url"
+            content="https://web3-albtian.vercel.app/"
+          />
+          <meta name="twitter:title" content="web3 playground" />
+          <meta
+            name="twitter:description"
+            content="a web3 playground for my personal projects from buildspace."
+          />
+          <meta
+            name="twitter:image"
+            content="https://web3-albtian.vercel.app/static/meta/ogImage.png"
+          />
         </Head>
         <body>
           {/* Make Color mode to persists when you refresh the page. */}
